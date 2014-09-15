35cb78a2f4a5eac28c0ed27b30fda7127e8d7c3e
Wanmuz <wanmuz86@gmail.com>
2014-09-15 11:59:25 +0800

    Add registration function

---
+++ GitExample/RegistrationViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegistrationViewController.h
+//  GitExample
+//
+//  Created by Azad Johari on 9/15/14.
+//  Copyright (c) 2014 Wan Muzaffar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegistrationViewController : UIViewController
+
+@end
